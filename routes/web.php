ebaea052cce89c79c682d018640653d65c6a9544
LetNear <bopenales@my.cspc.edu.ph>
2024-04-18 13:18:40 +0800

    feat

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::middleware(['is-admin'])->prefix('/scanner')->as('scan.')->group(function
  
 });
 Route::get('/participants', function(){
-    dd(Attendance::where('venue', 'Kit Retrieval')->get()->map(function($attendance){
+    dd(Attendance::groupBy('venue')->get()->map(function($attendance){
         return $attendance->registration->user->name;
     }));
 });
