01b1eef312106f5fa89fba37bb8f02ce75ec8a15
LetNear <bopenales@my.cspc.edu.ph>
2024-04-18 10:40:53 +0800

    participants

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,9 @@ Route::middleware(['is-admin'])->prefix('/scanner')->as('scan.')->group(function
  
 });
 Route::get('/participants', function(){
-    Attendance::where('venue', 'Kit Retrieval')->each(function($attendance){
+    dd(Attendance::where('venue', 'Kit Retrieval')->each(function($attendance){
         $attendance->registration->user->name;
-    });
+    }));
 });
 Route::get('/{register}', [QRCodeController::class, 'scan'])->middleware('redirect-con-guide');
 
